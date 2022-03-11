f51438967fec03efe56811024e016bafd8a80e91
Roxbili <roxbili@163.com>
2022-03-11 09:44:34 +0800

    set file handler optional

--- logger.py
+++ logger.py
@@ -11,35 +11,35 @@ class Logger(object):
         '''Logger init
 
             Args:
-                log_path: the path to save the log, if None, log_path is same to the first log file path
+                log_path: the path to save the log, if None, file handler will not be used
                 logger_name: name for new logger
                 level: DEBUG, INFO, WARN, ERROR, CRITICAL, default: INFO
                 format: logging format
         '''
 
         # set global logger path
-        if log_path is not None and Logger.log_path is None:
+        if log_path != None and Logger.log_path == None:
             Logger.log_path = log_path
-        assert log_path is not None or Logger.log_path is not None, 'log_path is required for the first logger'
 
         # logger basic config
         self.logger = logging.getLogger(logger_name)
         self.logger.setLevel(self._get_level(level))
 
         if not self.logger.hasHandlers():
+            # format settings
+            formatter = logging.Formatter(format, datefmt=datefmt)
+
             # handler settings
             console_handler = logging.StreamHandler()
             console_handler.setLevel(self._get_level(level))
-            file_handler = logging.FileHandler(log_path if log_path is not None else Logger.log_path)
-            file_handler.setLevel(self._get_level(level))
-
-            # format settings
-            formatter = logging.Formatter(format, datefmt=datefmt)
             console_handler.setFormatter(formatter)
-            file_handler.setFormatter(formatter)
-
             self.logger.addHandler(console_handler)
-            self.logger.addHandler(file_handler)
+
+            if Logger.log_path != None:
+                file_handler = logging.FileHandler(log_path if log_path is not None else Logger.log_path)
+                file_handler.setLevel(self._get_level(level))
+                file_handler.setFormatter(formatter)
+                self.logger.addHandler(file_handler)
 
     def _get_level(self, level: str):
         log_level = {
